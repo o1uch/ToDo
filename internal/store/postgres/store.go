18c2aed3d8f416983b2e1a339a1f2a6716943f0f
Pavel <newoluch@yandex.ru>
2026-05-19 21:33:09 +0200

    WIP: feature: добавить поддержку PostgreSQL хранилища - реализован метод Create из интерфейса TaskRepository

--- internal/store/postgres/store.go
+++ internal/store/postgres/store.go
@@ -1,6 +1,10 @@
 package postgres
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/o1uch/go_final_project/internal/store"
+)
 
 type SchedulerStore struct {
 	db *sql.DB
@@ -8,4 +12,21 @@ type SchedulerStore struct {
 
 func NewSchedulerStore(db *sql.DB) SchedulerStore {
 	return SchedulerStore{db: db}
+}
+
+func (s SchedulerStore) Create(task *store.Task) (int64, error) {
+	var pk int64
+
+	query := `INSERT INTO scheduler (date, title, comment, repeat) 
+	VALUES ($1, $2, $3, $4) 
+	RETURNING id`
+	// RETURNING для того, чтобы вернуть идентификатор вызывающей программе
+	// обратно в приложение Go.
+	err := s.db.QueryRow(query, task.Date, task.Title, task.Comment, task.Repeat).Scan(&pk)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return pk, nil
 }
